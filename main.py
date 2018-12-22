6a987d48b88a18795185cb33db8ded19329aaf0f
Andrew Schmidt <abschmidt6@gmail.com>
2018-12-22 14:22:34 -0500

    Changed params to try to get better example

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ img_ext = ".jpg"
 slicer = Slicer(dir_name, img_ext, "simple", False)
 slicer.slice()
 
-convex_slicer = Slicer(dir_name, img_ext, "convex", False, 4)
+convex_slicer = Slicer(dir_name, img_ext, "convex", False, 10)
 convex_slicer.slice()
 
-concave_slicer = Slicer(dir_name, img_ext, "concave", False, 4)
+concave_slicer = Slicer(dir_name, img_ext, "concave", False, 10)
 concave_slicer.slice()
